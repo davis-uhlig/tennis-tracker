776f6168bb17eab131cbcbf0d0a0119dcdf16a3e
Davis Uhlig <duhlig@Daviss-MacBook-Pro.local>
2018-01-05 16:57:28 -0500

    initial commit

---
+++ src/main/java/com/example/tennis/model/User.java
@@ -0,0 +1,96 @@
+package com.example.tennis.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by duhlig on 1/5/18.
+ */
+@Entity
+@Table(name="users")
+
+public class User {
+    @Id
+    @GeneratedValue
+    private int userId;
+
+    @Column(name="firstName")
+    private String fistName;
+
+    @Column(name="lastName")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    public User(String fistName, String lastName, String email, String password) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return getUserId() == user.getUserId();
+    }
+
+    @Override
+    public int hashCode() {
+        return getUserId();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", fistName='" + fistName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
